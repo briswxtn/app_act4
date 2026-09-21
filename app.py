cb9c3c7d2fedf6176d880721b5da76a6e7a607c1
briswxtn <brisscarrasco24@gmail.com>
2026-09-20 19:06:37 -0600

    Correct temperature input prompt and comparison operator
    Fix typo in temperature input prompt and correct comparison operator.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.title("Clasificador de temperatura")
 temperatura == st.number_input(
-  "Introducir temperatura en °C:".
+  "Introducir temperatura en °C: "
   value=20
 )
   if temperatura<20:
