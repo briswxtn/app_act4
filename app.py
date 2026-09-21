245b8dc72f3ed2c882ba76b138718bc59ad8c5c2
briswxtn <brisscarrasco24@gmail.com>
2026-09-20 19:02:27 -0600

    Fix comparison operators for temperature checks

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ temperatura = st.number_input(
   "Introducir temperatura en °C:".
   value=20
 )
-  if temperatura＜20:
+  if temperatura﹤20:
     st.writte("Hace frío")
-  elif temperatura＜30:
+  elif temperatura﹤30:
     st.writte("La temperatura es agradable")
   else:
   st.writte("Hace calor")
