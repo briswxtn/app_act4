c4dae8f9966bd4be04299f2a91ce5fd467dff89f
briswxtn <brisscarrasco24@gmail.com>
2026-09-20 18:19:24 -0600

    Fix temperature condition checks in app.py

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ temperatura = st.number_input(
   "Introducir temperatura en °C:".
   value=20
 )
-if temperatura＜=20:
-st.writte("Hace frío")
-elif temperatura＜=30:
-st.writte("La temperatura es agradable")
+
+if temperatura＜20:
+  st.writte("Hace frío")
+elif temperatura＜30:
+  st.writte("La temperatura es agradable")
 else:
-st.writte("Hace calor")
+  st.writte("Hace calor")
