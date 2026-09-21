a90433c07fbc5d45e6daeefb218107a8d5e9d533
briswxtn <brisscarrasco24@gmail.com>
2026-09-20 19:08:34 -0600

    Correct temperature input and condition checks
    Fix syntax errors in temperature classification logic.

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 st.title("Clasificador de temperatura")
-temperatura == st.number_input("Introducir temperatura en °C: "
+temperatura = st.number_input(
+  "Introducir temperatura en °C: ",
   value=20
 )
-  if temperatura<20:
+  if temperatura < 20:
     st.writte("Hace frío")
-  elif temperatura<30:
+  elif temperatura < 30:
     st.writte("La temperatura es agradable")
   else:
   st.writte("Hace calor")
